4a4b6a0e4576b1d7430674e5000ce3142aad2abe
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2019-06-25 01:39:22 +0800

    feat: serverless target

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,6 @@
 const withTypescript = require("@zeit/next-typescript");
 const withCSS = require("@zeit/next-css");
 
-module.exports = withTypescript(withCSS());
+module.exports = withTypescript(withCSS({
+  target: "serverless"
+}));
