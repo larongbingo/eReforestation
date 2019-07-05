696235636c7ac099755cff5d719600c78a4e1dfb
Renz Christen Yeomer A. Pagulayan <rcyapagulayan@gmail.com>
2019-07-05 08:40:02 +0800

    feat: default page layout on Root component

--- src/client/src/index.tsx
+++ src/client/src/index.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { FunctionComponent } from "react";
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as serviceWorker from './serviceWorker';
+import { PageNavbar } from "./components/PageNavbar";
+import { Routes } from "./Routes";
+
+import "bootstrap/dist/css/bootstrap.css";
+
+export const Root: FunctionComponent = () => (
+  <>
+    <PageNavbar />
+    <Routes />
+  </>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
