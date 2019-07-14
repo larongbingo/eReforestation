d78f1db4a2441e0f77630847cc09cb5a8fa0c1ce
Renz Christen Yeomer A. Pagulayan <rcyapagulayan@gmail.com>
2019-07-14 13:49:10 +0800

    build: moving old pages
    Moving pages from old laptop

--- src/client/src/components/PageNavbar.tsx
+++ src/client/src/components/PageNavbar.tsx
@@ -4,7 +4,7 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 export const PageNavbar: FunctionComponent = () => (
   <Navbar bg="dark" variant="dark" expand="lg">
     <Container>
-      <Navbar.Brand href="#home">eReforestation</Navbar.Brand>
+      <Navbar.Brand href="/">eReforestation</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
