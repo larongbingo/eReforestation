3c1b9f492096cb3ce81275e9ba6d99336df90e5c
Renz Christen Yeomer A. Pagulayan <renz.pagulayan@newtrends.com.ph>
2019-07-15 09:22:01 +0800

    feat: default value for permission

--- src/server/src/modules/database/models/Permission.Model.ts
+++ src/server/src/modules/database/models/Permission.Model.ts
@@ -1,4 +1,4 @@
-import { Model, Table, Column, DataType } from "sequelize-typescript";
+import { Model, Table, Column, DataType, Default } from "sequelize-typescript";
 
 import { IPermissions, UserPermissions } from "../../../../../interfaces/models/IPermissions";
 
@@ -10,6 +10,7 @@ export class Permission extends Model<Permission> implements IPermissions {
   @Column(DataType.STRING)
   userId?: string;
 
+  @Default(UserPermissions.Participant)
   @Column(DataType.STRING)
   permission: UserPermissions;
 }
