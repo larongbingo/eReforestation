02effcd639179d03c4366ddec1d59a3c3b6a5116
Renz Christen Yeomer A. Pagulayan <renz.pagulayan@newtrends.com.ph>
2019-07-17 14:49:27 +0800

    feat: import UserService and ContactPerson Modules

--- src/server/src/modules/user/User.Module.ts
+++ src/server/src/modules/user/User.Module.ts
@@ -1,8 +1,14 @@
 import { Module } from "@nestjs/common";
 
+import { ContactPersonModule } from "./modules/contact.person/ContactPerson.Module";
+import { UserDetailsModule } from "./modules/user.details/UserDetails.Module";
 import { UserServiceProvider } from "./User.Service";
 
 @Module({
+  imports: [
+    ContactPersonModule,
+    UserDetailsModule,
+  ],
   providers: [
     UserServiceProvider,
   ],
