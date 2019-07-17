8b7dd2dfcd37a8c72bac98926e207fb8268aea44
Renz Christen Yeomer A. Pagulayan <renz.pagulayan@newtrends.com.ph>
2019-07-17 14:49:58 +0800

    feat: endpoint for login

--- src/client/src/config/endpoints.ts
+++ src/client/src/config/endpoints.ts
@@ -1,6 +1,6 @@
 
-export const APIS_ENDPOINT_ROOT = "localhost:3000";
+export const APIS_ENDPOINT_ROOT = "http://localhost:8080";
 
 export const APIS_ENDPOINTS = {
-
+  login: `${APIS_ENDPOINT_ROOT}/auth/login`,
 };
