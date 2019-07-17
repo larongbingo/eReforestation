605d205d62b0ce6ff8273ea51f8b8e5a489c7334
Renz Christen Yeomer A. Pagulayan <renz.pagulayan@newtrends.com.ph>
2019-07-17 17:58:04 +0800

    feat(user-service): crud feats

--- src/interfaces/services/IUserService.ts
+++ src/interfaces/services/IUserService.ts
@@ -4,4 +4,7 @@ export const IUserService = "IUserService";
 export interface IUserService {
   findOneByUsername(username: string): Promise<IUser>;
   findOneById(id: string): Promise<IUser>;
+  createUser(details: IUser): Promise<IUser>;
+  destroyUser(userId: string): Promise<boolean>;
+  updateUser(userId: string, newDetails: Partial<IUser>): Promise<IUser>;
 }
--- src/server/src/modules/user/User.Service.ts
+++ src/server/src/modules/user/User.Service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Provider } from "@nestjs/common";
+import { Injectable, Provider, BadRequestException } from "@nestjs/common";
 
 import { IUserService } from "../../../../interfaces/services/IUserService";
 import { IUser } from "../../../../interfaces/models/IUser";
@@ -6,6 +6,25 @@ import { User } from "../database/models/User.Model";
 
 @Injectable()
 export class UserService implements IUserService {
+  public async createUser(details: IUser): Promise<IUser> {
+    const user = await User.find({where: {username: details.username}});
+    return user;
+  }
+
+  public async destroyUser(userId: string): Promise<boolean> {
+    const user = await User.findOne({where: {id: userId}});
+    if (!user) { return false; }
+    await user.destroy();
+    return true;
+  }
+
+  public async updateUser(userId: string, newDetails: Partial<IUser>): Promise<IUser> {
+    const user = await User.findOne({where: {id: userId}});
+    if (!user) { throw new BadRequestException("You do not have an account, please create one."); }
+    Object.keys(newDetails).forEach(key => user[key] = newDetails[key]);
+    return user.save();
+  }
+
   public async findOneById(id: string): Promise<IUser> {
     return await User.findOne({where: {id}});
   }
