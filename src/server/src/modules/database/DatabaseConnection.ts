3a94916a127888f7fb1864f92b3da6f3526de4c9
Renz Christen Yeomer A. Pagulayan <renz.pagulayan@newtrends.com.ph>
2019-07-17 14:32:35 +0800

    feat: load new models into connection

--- src/server/src/modules/database/DatabaseConnection.ts
+++ src/server/src/modules/database/DatabaseConnection.ts
@@ -3,7 +3,11 @@ import { Sequelize } from "sequelize-typescript";
 
 import { ContactPerson } from "./models/ContactPerson.Model";
 import { Event } from "./models/Event.Model";
+import { Log } from "./models/Log.Model";
+import { News } from "./models/News.Model";
+import { Permission } from "./models/Permission.Model";
 import { User } from "./models/User.Model";
+import { UserDetails } from "./models/UserDetails.Model";
 import { DatabaseConnectionConfig } from "./DatabaseConnectionConfig";
 
 export const DatabaseConnectionKey = "DatabaseConnection";
@@ -12,7 +16,11 @@ export const DatabaseConnection = new Sequelize(new DatabaseConnectionConfig());
 DatabaseConnection.addModels([
   ContactPerson,
   Event,
+  Log,
+  News,
+  Permission,
   User,
+  UserDetails,
 ]);
 
 export const DatabaseConnectionProvider: Provider = {
