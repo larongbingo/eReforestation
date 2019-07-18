e29c1f3972e7dbc65fdda9d9a735821dfc2791e9
Renz Christen Yeomer A. Pagulayan <renz.pagulayan@newtrends.com.ph>
2019-07-18 14:11:27 +0800

    feat: import user controller to module

--- src/server/src/modules/user/User.Module.ts
+++ src/server/src/modules/user/User.Module.ts
@@ -2,12 +2,16 @@ import { Module } from "@nestjs/common";
 
 import { ContactPersonModule } from "./modules/contact.person/ContactPerson.Module";
 import { UserDetailsModule } from "./modules/user.details/UserDetails.Module";
+import { UserContoller } from "./User.Controller";
 import { UserServiceProvider } from "./User.Service";
 
 @Module({
   imports: [
     ContactPersonModule,
     UserDetailsModule,
+  ],
+  controllers: [
+    UserContoller,
   ],
   providers: [
     UserServiceProvider,
