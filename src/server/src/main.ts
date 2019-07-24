f4dbcd3b99833876b4ea8f5a95600e88f820aca3
Renz Christen Yeomer A. Pagulayan <renz.pagulayan@newtrends.com.ph>
2019-07-24 15:17:41 +0800

    feat: allow cors and security on express

--- src/server/src/main.ts
+++ src/server/src/main.ts
@@ -1,11 +1,13 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import helmet = require("helmet");
 
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ skipMissingProperties: true }));
+  app.use(helmet());
   await app.listen(3000);
 }
 bootstrap();
