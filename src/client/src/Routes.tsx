4d7eb4a8c58da040ee155285c79c1f2eb6e8997f
Renz Christen Yeomer A. Pagulayan <renz.pagulayan@newtrends.com.ph>
2019-07-25 12:56:41 +0800

    feat: user profile page

--- src/client/src/Routes.tsx
+++ src/client/src/Routes.tsx
@@ -1,12 +1,15 @@
 import React, { FunctionComponent } from "react";
 import { Route, BrowserRouter } from "react-router-dom";
 
+import { PrivateRoute } from "./components/PrivateRoute";
 import { Index } from "./pages/index";
 import { Events } from "./pages/events";
 import { LogIn } from "./pages/login";
+import  { ProfilePage } from "./pages/profilePage";
 
 export const Routes: FunctionComponent = () => (
   <BrowserRouter>
+    <PrivateRoute path="/profile" component={ProfilePage} exact />
     <Route path="/login" component={LogIn} exact />
     <Route path="/events" component={Events} exact />
     <Route path="/" component={Index} exact />
