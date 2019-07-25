1806e8994ab0a432bbea6c006a279de7419aa381
Renz Christen Yeomer A. Pagulayan <renz.pagulayan@newtrends.com.ph>
2019-07-25 16:45:09 +0800

    feat: components for news page

---
+++ src/client/src/components/news/NewsListEntry.tsx
@@ -0,0 +1,14 @@
+import React, { FunctionComponent } from "react";
+
+import { INews } from "../../../../interfaces/models/INews";
+
+export const NewsListEntry: FunctionComponent<NewsListEntryProps> = ({news}) => (
+  <tr>
+    <td>{new Date(news.createdAt).toLocaleDateString()}</td>
+    <td>{news.headline}</td>
+  </tr>
+);
+
+export interface NewsListEntryProps {
+  news: INews
+}
