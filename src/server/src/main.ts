b8e8ca6b7a200ff96615cf1140da07ebe49488f1
Renz Christen Yeomer A. Pagulayan <renz.pagulayan@newtrends.com.ph>
2019-07-26 17:15:03 +0800

    feat: added compression and cors

--- src/server/src/main.ts
+++ src/server/src/main.ts
@@ -1,13 +1,17 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import * as compression from "compression";
 import helmet = require("helmet");
+import cors = require("cors");
 
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ skipMissingProperties: true }));
+  app.useGlobalPipes(new ValidationPipe({ skipMissingProperties: true, transform: true }));
   app.use(helmet());
-  await app.listen(3000);
+  app.use(cors());
+  app.use(compression());
+  await app.listen(8080);
 }
 bootstrap();
