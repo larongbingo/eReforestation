f67fd30659d3b8abf72b00bb9ad393c31d71bba1
Renz Christen Yeomer A. Pagulayan <renz.pagulayan@newtrends.com.ph>
2019-07-26 15:13:10 +0800

    feat: ignore typings

--- src/client/src/components/news/NewsListEntry.tsx
+++ src/client/src/components/news/NewsListEntry.tsx
@@ -4,7 +4,7 @@ import { INews } from "../../../../interfaces/models/INews";
 
 export const NewsListEntry: FunctionComponent<NewsListEntryProps> = ({news}) => (
   <tr>
-    <td>{new Date(news.createdAt).toLocaleDateString()}</td>
+    <td>{new Date(news.createdAt!).toLocaleDateString()}</td>
     <td>{news.headline}</td>
   </tr>
 );
