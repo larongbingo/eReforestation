12d69913406cdc7d9b258aa8516f6945d6bc5ac3
Renz Christen Yeomer A. Pagulayan <renz.pagulayan@newtrends.com.ph>
2019-07-26 13:05:51 +0800

    feat(routes): added news detail and events detail
    Added news details and events details to React Router

--- src/client/src/Routes.tsx
+++ src/client/src/Routes.tsx
@@ -6,12 +6,16 @@ import { Index } from "./pages/index";
 import { Events } from "./pages/events";
 import { LogIn } from "./pages/login";
 import  { ProfilePage } from "./pages/profilePage";
+import { NewsListPage } from "./pages/news";
+import { NewsDetailsPage } from "./pages/newsDetails";
 
 export const Routes: FunctionComponent = () => (
   <BrowserRouter>
     <PrivateRoute path="/profile" component={ProfilePage} exact />
     <Route path="/login" component={LogIn} exact />
     <Route path="/events" component={Events} exact />
+    <Route path="/news" component={NewsListPage} exact />
+    <Route path="/news/:newsId" component={NewsDetailsPage} exact />
     <Route path="/" component={Index} exact />
   </BrowserRouter>
 );
