35493d28b3057a89e5d010e5f17bafd63fed4d59
Renz Christen Yeomer A. Pagulayan <larongbingo@gmail.com>
2019-07-28 12:58:52 +0800

    feat: added auth guard

--- src/server/src/modules/user/modules/contact.person/ContactPerson.Controller.ts
+++ src/server/src/modules/user/modules/contact.person/ContactPerson.Controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Inject, Get, Put, Post, Body } from "@nestjs/common";
+import { Controller, Inject, Get, Put, Post, Body, UseGuards } from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
 
 import { IUser } from "../../../../../../interfaces/models/IUser";
 import { IContactPersonService } from "../../../../../../interfaces/services/IContactPersonService";
@@ -14,18 +15,21 @@ export class ContactPersonController {
   ) {}
 
   @Get()
+  @UseGuards(AuthGuard("bearer"))
   public async getContactPerson(@UserEntity() user: IUser) {
     const contactPersonDetails = await this.contactPersonService.getContactPerson(user.id);
     return {iat: Date.now(), contactPersonDetails};
   }
 
   @Post()
+  @UseGuards(AuthGuard("bearer"))
   public async createContactPerson(@Body() contactPersonCreateDto: ContactPersonCreateDto, @UserEntity() user: IUser) {
     const contactPersonDetails = await this.contactPersonService.createContactPerson(contactPersonCreateDto, user.id);
     return {iat: Date.now(), contactPersonDetails};
   }
 
   @Put()
+  @UseGuards(AuthGuard("bearer"))
   public async updateContactPerson(@Body() contactPersonUpdateDto: ContactPersonUpdateDto, @UserEntity() user: IUser) {
     const contactPersonDetails = await this.contactPersonService.updateContactPerson(contactPersonUpdateDto, user.id);
     return {iat: Date.now(), contactPersonDetails};
