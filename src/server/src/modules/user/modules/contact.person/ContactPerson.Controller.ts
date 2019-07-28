131566ef313b5bfc7e01e6972f94d92ea87db67b
Renz Christen Yeomer A. Pagulayan <larongbingo@gmail.com>
2019-07-28 16:47:52 +0800

    fix: incorrect http methods

--- src/server/src/modules/user/modules/contact.person/ContactPerson.Controller.ts
+++ src/server/src/modules/user/modules/contact.person/ContactPerson.Controller.ts
@@ -21,14 +21,14 @@ export class ContactPersonController {
     return {iat: Date.now(), contactPersonDetails};
   }
 
-  @Post()
+  @Put()
   @UseGuards(AuthGuard("bearer"))
   public async createContactPerson(@Body() contactPersonCreateDto: ContactPersonCreateDto, @UserEntity() user: IUser) {
     const contactPersonDetails = await this.contactPersonService.createContactPerson(contactPersonCreateDto, user.id);
     return {iat: Date.now(), contactPersonDetails};
   }
 
-  @Put()
+  @Post()
   @UseGuards(AuthGuard("bearer"))
   public async updateContactPerson(@Body() contactPersonUpdateDto: ContactPersonUpdateDto, @UserEntity() user: IUser) {
     const contactPersonDetails = await this.contactPersonService.updateContactPerson(contactPersonUpdateDto, user.id);
