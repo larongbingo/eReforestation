145ef4176f9cdf265b1b160a4f35d66ba5a1f3e3
Renz Christen Yeomer A. Pagulayan <larongbingo@gmail.com>
2019-07-31 11:21:17 +0800

    fix: permission service not exported

--- src/server/src/modules/permissions/Permission.Module.ts
+++ src/server/src/modules/permissions/Permission.Module.ts
@@ -5,5 +5,6 @@ import { PermissionServiceProvider } from "./Permission.Service";
 @Module({
   imports: [],
   providers: [PermissionServiceProvider],
+  exports: [PermissionServiceProvider]
 })
 export class PermissionModule {}
