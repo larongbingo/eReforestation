38f3abd34b9f6da6afe404644f773597a3eca8da
Renz Christen Yeomer A. Pagulayan <larongbingo@gmail.com>
2019-08-03 20:07:20 +0800

    feat: links to actual govt websites

--- src/client/src/components/Footer/GovLinks.tsx
+++ src/client/src/components/Footer/GovLinks.tsx
@@ -3,9 +3,9 @@ import React, { FunctionComponent } from "react";
 export const GovLinks: FunctionComponent = () => (
   <>
     <h5>Gov Links</h5>
-    <a href="/">Gov Ph</a> <br/>
-    <a>Cavite Ph</a> <br/>
-    <a>CHED Ph</a> <br/>
-    <a>DENR</a>
+    <a href="https://www.gov.ph">Gov Ph</a> <br/>
+    <a href="http://cavite.gov.ph">Cavite Ph</a> <br/>
+    <a href="https://www.denr.gov.ph">DENR Ph</a> <br/>
+    <a href="http://calabarzon.denr.gov.ph">DENR Calabarzon</a>
   </>
 );
