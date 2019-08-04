451e5b4c76002355b4c834f1eac10b579d3f670a
Renz Christen Yeomer A. Pagulayan <larongbingo@gmail.com>
2019-08-04 12:35:03 +0800

    feat: penro cavite contact details

---
+++ src/client/src/config/penroCaviteContacts.ts
@@ -0,0 +1,15 @@
+
+export const PENRO_CAVITE_CONTACTS = {
+  address: "DENR PENRO Cavite Office Bldg., Brgy. Gregorio, Trece Martires City, Cavite",
+  socialMedias: {
+    facebook: "https://www.facebook.com/denr.penrocavite",
+    instagram: "penrocaviteofficial"
+  },
+  emails: [
+    "penrocavite@yahoo.com",
+    "denrcavite@gmail.com",
+  ],
+  contactNumbers: [
+    09081943573,
+  ],
+};
