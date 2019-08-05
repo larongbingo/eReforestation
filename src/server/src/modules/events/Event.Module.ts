a17814bf2bd5b451ea38f453e6e9b4e1eb9abd69
Renz Christen Yeomer A. Pagulayan <renz.pagulayan@newtrends.com.ph>
2019-08-05 16:18:28 +0800

    feat: circular dep to event participant module

--- src/server/src/modules/events/Event.Module.ts
+++ src/server/src/modules/events/Event.Module.ts
@@ -1,12 +1,13 @@
-import { Module } from "@nestjs/common";
+import { Module, forwardRef } from "@nestjs/common";
 
 import { PermissionModule } from "../permissions/Permission.Module";
 
+import { EventParticipationModule } from "./modules/eventParticipants/EventParticipants.Module";
 import { EventController } from "./Event.Controller";
 import { EventService } from "./Event.Service";
 
 @Module({
-  imports: [PermissionModule],
+  imports: [PermissionModule, forwardRef(() => EventParticipationModule)],
   controllers: [EventController],
   providers: [EventService],
   exports: [EventService],
