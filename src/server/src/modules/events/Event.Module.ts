b01a65bb23d53886035f287b0560dd595f2257da
Renz Christen Yeomer A. Pagulayan <renz.pagulayan@newtrends.com.ph>
2019-08-06 08:26:24 +0800

    fix: used the class instead of the provider

--- src/server/src/modules/events/Event.Module.ts
+++ src/server/src/modules/events/Event.Module.ts
@@ -4,12 +4,12 @@ import { PermissionModule } from "../permissions/Permission.Module";
 
 import { EventParticipationModule } from "./modules/eventParticipants/EventParticipants.Module";
 import { EventController } from "./Event.Controller";
-import { EventService } from "./Event.Service";
+import { EventServiceProvider } from "./Event.Service";
 
 @Module({
   imports: [PermissionModule, forwardRef(() => EventParticipationModule)],
   controllers: [EventController],
-  providers: [EventService],
-  exports: [EventService],
+  providers: [EventServiceProvider],
+  exports: [EventServiceProvider],
 })
 export class EventModule {}
