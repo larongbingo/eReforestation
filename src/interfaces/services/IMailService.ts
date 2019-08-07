321e6577843147f452be1c5cd39d419f20421518
Renz Christen Yeomer A. Pagulayan <renz.pagulayan@newtrends.com.ph>
2019-08-07 10:02:33 +0800

    feat: contact retrieval service

--- src/interfaces/services/IMailService.ts
+++ src/interfaces/services/IMailService.ts
@@ -4,8 +4,18 @@ export interface IMailService {
   sendMail(mail: IMail): Promise<any>;
 }
 
+export const IContactRetrievalService = "IContactRetrievalService";
+export interface IContactRetrievalService {
+  retrieveContacts(userId: string): Promise<IContact>;
+}
+
 export interface IMail {
   to: string;
   title: string;
   message: string;
+}
+
+export interface IContact {
+  emailAddress: string; 
+  phoneNumber: string;
 }
--- src/server/src/modules/mail/Mail.Module.ts
+++ src/server/src/modules/mail/Mail.Module.ts
@@ -1,9 +1,13 @@
 import { Module } from "@nestjs/common";
 
+import { UserDetailsModule } from "../user/modules/user.details/UserDetails.Module";
+
 import { MailServiceProvider } from "./Mail.Service";
+import { ContactRetrievalServiceProvider } from "./ContactRetrieval.Service";
 
 @Module({
-  providers: [MailServiceProvider],
-  exports: [MailServiceProvider],
+  imports: [UserDetailsModule],
+  providers: [MailServiceProvider, ContactRetrievalServiceProvider],
+  exports: [MailServiceProvider, ContactRetrievalServiceProvider],
 })
 export class MailModule {}
