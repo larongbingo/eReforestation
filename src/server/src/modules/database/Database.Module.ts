9340824c4d6ffc317cdd78a1baaf379c0f28fb66
Renz Christen Yeomer A. Pagulayan <renz.pagulayan@newtrends.com.ph>
2019-08-07 12:29:29 +0800

    feat: close database conn on app close

--- src/server/src/modules/database/Database.Module.ts
+++ src/server/src/modules/database/Database.Module.ts
@@ -1,11 +1,25 @@
-import { Module } from "@nestjs/common";
+import { Module, OnModuleDestroy, OnApplicationShutdown } from "@nestjs/common";
 import { DatabaseConnectionConfig } from "./DatabaseConnectionConfig";
-import { DatabaseConnectionProvider } from "./DatabaseConnection";
+import { DatabaseConnectionProvider, DatabaseConnection } from "./DatabaseConnection";
 
 @Module({
   providers: [DatabaseConnectionConfig, DatabaseConnectionProvider],
   exports: [DatabaseConnectionProvider],
 })
-export class DatabaseModule {}
+export class DatabaseModule implements OnModuleDestroy, OnApplicationShutdown {
+
+  public async onApplicationShutdown(signal?: string) {
+    await this.closeDatabaseConnection();
+  }
+
+  public async onModuleDestroy() {
+    await this.closeDatabaseConnection();
+  }
+
+  private async closeDatabaseConnection() {
+    return DatabaseConnection.close();
+  }
+
+}
 
 export default DatabaseModule;
