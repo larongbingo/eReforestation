764243ba8b8b1d060cbe73d4b1b8a7d63e0f75a2
Renz Christen Yeomer A. Pagulayan <renz.pagulayan@newtrends.com.ph>
2019-08-08 14:08:18 +0800

    feat: get logs method

--- src/interfaces/services/ILogService.ts
+++ src/interfaces/services/ILogService.ts
@@ -1,5 +1,8 @@
+import { ILog } from "../models/ILog";
 
 export const ILogService = "ILogService";
 export interface ILogService {
   log(event: string, description: string, param?: string): Promise<void>;
+
+  getLogs(): Promise<ILog[]>;
 }
--- src/server/src/modules/log/Log.Service.ts
+++ src/server/src/modules/log/Log.Service.ts
@@ -1,10 +1,14 @@
 import { Injectable, Provider } from "@nestjs/common";
 
 import { ILogService } from "../../../../interfaces/services/ILogService";
+import { ILog } from "../../../../interfaces/models/ILog";
 import { Log } from "../database/models/Log.Model";
 
 @Injectable()
 export class LogService implements ILogService {
+  public async getLogs(): Promise<ILog[]> {
+    return Log.findAll();
+  }
 
   public async log(event: string, description: string, params?: string): Promise<void> {
     Log.create({event, description, params});
