164effac61f202443d6d7b48ebff5c4fb9effb10
Renz Christen Yeomer A. Pagulayan <larongbingo@gmail.com>
2019-08-15 07:34:00 +0800

    test: event participants controller spec

--- src/server/src/modules/events/modules/eventParticipants/tests/event-participants-controller.spec.ts
+++ src/server/src/modules/events/modules/eventParticipants/tests/event-participants-controller.spec.ts
@@ -1,17 +1,79 @@
-import { IEventService } from "../../../../../../../interfaces/services/IEventService";
+import { IUser } from "../../../../../../../interfaces/models/IUser";
+import { IEventParticipants } from "../../../../../../../interfaces/models/IEventParticipants";
+import { IEventParticipantsService } from "../../../../../../../interfaces/services/IEventParticipantsService";
 import { EventParticipantsController } from "../EventParticipants.Controller";
 
-describe("EventParticipants (Unit)", () => {
+const testingUser: IUser = {
+  id: "testing",
+  username: "testing",
+  password: "testing",
+};
+
+const testingEventParticipation: IEventParticipants = {
+  id: "testing",
+  userId: "testing",
+  eventId: "testing",
+  confirmed: true,
+};
+
+const mockedIEventParticipantsService: IEventParticipantsService = {
+  joinEvent: jest.fn().mockResolvedValue("testing"),
+  leaveEvent: jest.fn().mockResolvedValue(null),
+  findOneById: jest.fn().mockResolvedValue(testingEventParticipation),
+  updateOneById: jest.fn().mockResolvedValue(testingEventParticipation),
+};
+
+describe("EventParticipantsController (Unit)", () => {
+
+  const sut = new EventParticipantsController(mockedIEventParticipantsService);
 
   describe("joinEvent", () => {
 
-    it("should call EventParticipantService.joinEvent()");
+    it("should return a string when called", async () => {
+
+      // Arrange
+      const eventId = testingEventParticipation.eventId;
+      const user = { ...testingUser };
+
+      // Act
+      const result = await sut.joinEvent(eventId, user);
+
+      // Assert
+      expect(result.confirmationString).toEqual("testing");
+
+    });
+
+    it("should call EventParticipantService.joinEvent()", async () => {
+
+      // Arrange
+      const eventId = testingEventParticipation.eventId;
+      const user = { ...testingUser };
+
+      // Act
+      await sut.joinEvent(eventId, user);
+
+      // Assert
+      expect(mockedIEventParticipantsService.joinEvent).toBeCalled();
+
+    });
 
   });
 
   describe("leaveEvent", () => {
 
-    it("should call EventParticipantService.leaveEvent()");
+    it("should call EventParticipantService.leaveEvent()", async () => {
+
+      // Arrange
+      const eventId = testingEventParticipation.eventId;
+      const user = { ...testingUser };
+
+      // Act
+      await sut.leaveEvent(eventId, user);
+
+      // Assert
+      expect(mockedIEventParticipantsService.leaveEvent).toBeCalled();
+
+    });
 
   });
 
