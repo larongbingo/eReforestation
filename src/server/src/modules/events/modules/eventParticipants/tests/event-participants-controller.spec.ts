b2ada174777226bc1f6be819520c1178858c0c18
Renz Christen Yeomer A. Pagulayan <renz.pagulayan@newtrends.com.ph>
2019-08-14 15:49:55 +0800

    chore: moving files

---
+++ src/server/src/modules/events/modules/eventParticipants/tests/event-participants-controller.spec.ts
@@ -0,0 +1,18 @@
+import { IEventService } from "../../../../../../../interfaces/services/IEventService";
+import { EventParticipantsController } from "../EventParticipants.Controller";
+
+describe("EventParticipants (Unit)", () => {
+
+  describe("joinEvent", () => {
+
+    it("should call EventParticipantService.joinEvent()");
+
+  });
+
+  describe("leaveEvent", () => {
+
+    it("should call EventParticipantService.leaveEvent()");
+
+  });
+
+});
