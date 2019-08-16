5980a4c7907ac12afdc5ef14e0994021fe9bc157
Renz Christen Yeomer A. Pagulayan <larongbingo@gmail.com>
2019-08-16 18:48:27 +0800

    feat: implemented admin mailing service

--- src/server/src/modules/superuser/modules/admin/AdminMailing.Service.ts
+++ src/server/src/modules/superuser/modules/admin/AdminMailing.Service.ts
@@ -10,20 +10,43 @@ export class AdminMailingService implements IAdminMailingService {
     @Inject(IContactRetrievalService) private readonly contactRetrievalService: IContactRetrievalService,
   ) {}
 
+  // TODO: Add a param for the id of the event
   public async mailUserOnSuccessfulApplication(userId: string): Promise<void> {
-    throw new Error("Method not implemented.");
+    const contact = await this.contactRetrievalService.retrieveContacts(userId);
+    this.mailService.sendMail({
+      to: contact.emailAddress,
+      title: "Event Application Approved",
+      message: "Your application to join an event has been approved.",
+    });
   }
 
+  // TODO: Add a param for the id of the event
   public async mailUserOnFailedApplication(userId: string): Promise<void> {
-    throw new Error("Method not implemented.");
+    const contact = await this.contactRetrievalService.retrieveContacts(userId);
+    this.mailService.sendMail({
+      to: contact.emailAddress,
+      title: "Event Application Rejected",
+      message: "Your application to join an event has been rejected.",
+    });
   }
 
+  // TODO: Add a param for the reason of the ban
   public async mailUserOnBan(userId: string): Promise<void> {
-    throw new Error("Method not implemented.");
+    const contact = await this.contactRetrievalService.retrieveContacts(userId);
+    this.mailService.sendMail({
+      to: contact.emailAddress,
+      title: "eReforestation Account Banned",
+      message: "Your account has been banned.",
+    });
   }
 
   public async mailUserOnUnban(userId: string): Promise<void> {
-    throw new Error("Method not implemented.");
+    const contact = await this.contactRetrievalService.retrieveContacts(userId);
+    this.mailService.sendMail({
+      to: contact.emailAddress,
+      title: "eReforestation Account Unbanned",
+      message: "Your account has been unbanned.",
+    });
   }
 
 }
