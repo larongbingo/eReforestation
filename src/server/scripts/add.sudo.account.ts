8339c4dab8eccedf3d19db81893b17e68c784dc1
Renz Christen Yeomer A. Pagulayan <larongbingo@gmail.com>
2019-09-17 20:03:44 +0800

    feat: add user details and contact person on sudo account

--- src/server/scripts/add.sudo.account.ts
+++ src/server/scripts/add.sudo.account.ts
@@ -2,11 +2,38 @@ import "../src/modules/database/DatabaseConnection";
 import { User } from "../src/modules/database/models/User.Model";
 import { Permission } from "../src/modules/database/models/Permission.Model";
 import { UserPermissions } from "../../interfaces/models/IPermissions";
+import { IContactPerson } from "../../interfaces/models/IContactPerson";
+import { IUserDetails } from "../../interfaces/models/IUserDetails";
+import { UserDetails } from "../src/modules/database/models/UserDetails.Model";
+import { ContactPerson } from "../src/modules/database/models/ContactPerson.Model";
 
 (async function() {
+  const adm = "Admin";
+  const userDetails: IUserDetails = {
+    firstName: adm,
+    middleName: adm,
+    lastName: adm,
+    // @ts-ignore
+    dateOfBirth: "05-05-1998",
+    address: adm,
+    emailAddress: "admin@admin.com",
+    phoneNumber: "123123123"
+  };
+
+  const contactPerson: IContactPerson = {
+    firstName: adm,
+    middleName: adm,
+    lastName: adm,
+    address: adm,
+    phoneNumber: "123123123123",
+    emailAddress: "admin@admin.com",
+  };
+
   try {
     const user = await User.create({username: "admin123", password: "admin123"});
     await Permission.create({userId: user.id, permission: UserPermissions.Superuser});
+    await UserDetails.create({...userDetails, userId: user.id});
+    await ContactPerson.create({...contactPerson, userId: user.id});
     process.exit(0);
   }
   catch(err) {
