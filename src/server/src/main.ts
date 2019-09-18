083b9955e26736f11a7c0a9e52e71273f7fd8c01
Renz Christen Yeomer A. Pagulayan <larongbingo@gmail.com>
2019-09-18 19:02:26 +0800

    feat: allow large files to be uploaded

--- src/server/src/main.ts
+++ src/server/src/main.ts
@@ -11,6 +11,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ skipMissingProperties: true, transform: true }));
   app.use("/images", express.static("static"));
+  app.use(express.json({limit: "100mb"}));
+  app.use(express.urlencoded({limit: "100mb", extended: true}));
   app.use(helmet());
   app.use(cors());
   app.use(compression());
