e51e4fa5cd97e5d856760e2c0fb9cf3102c8b332
Renz Christen Yeomer A. Pagulayan <larongbingo@gmail.com>
2019-09-19 22:25:32 +0800

    feat: import config module

--- src/server/src/modules/database/Database.Module.ts
+++ src/server/src/modules/database/Database.Module.ts
@@ -1,27 +1,15 @@
 import { Module, OnModuleDestroy, OnApplicationShutdown } from "@nestjs/common";
-import { DatabaseConnectionConfig } from "./DatabaseConnectionConfig";
-import { DatabaseConnectionProvider, DatabaseConnection } from "./DatabaseConnection";
-import { ServiceDatabaseConnectionProvider, ServiceDatabaseConnection } from "./ServiceDatabase.Connection";
+
+import { ConfigModule } from "../config/Config.Module";
+
+import { DatabaseConnection } from "./DatabaseConnection";
+import { ServiceDatabaseConnection } from "./ServiceDatabase.Connection";
 
 @Module({
-  providers: [DatabaseConnectionConfig, DatabaseConnectionProvider, ServiceDatabaseConnectionProvider],
-  exports: [DatabaseConnectionProvider],
+  imports: [ConfigModule],
+  providers: [DatabaseConnection, ServiceDatabaseConnection],
+  exports: [DatabaseConnection],
 })
-export class DatabaseModule implements OnModuleDestroy, OnApplicationShutdown {
-
-  public async onApplicationShutdown(signal?: string) {
-    await this.closeDatabaseConnection();
-  }
-
-  public async onModuleDestroy() {
-    await this.closeDatabaseConnection();
-  }
-
-  private async closeDatabaseConnection() {
-    await ServiceDatabaseConnection.close();
-    return DatabaseConnection.close();
-  }
-
-}
+export class DatabaseModule {}
 
 export default DatabaseModule;
