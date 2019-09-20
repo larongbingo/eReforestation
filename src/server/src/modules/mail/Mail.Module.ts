fe3195117e7b655bd934a18026fc63b597c212df
Renz Christen Yeomer A. Pagulayan <larongbingo@gmail.com>
2019-09-20 15:18:53 +0800

    chore: import config module into mail module

--- src/server/src/modules/mail/Mail.Module.ts
+++ src/server/src/modules/mail/Mail.Module.ts
@@ -1,12 +1,13 @@
 import { Module } from "@nestjs/common";
 
 import { UserDetailsModule } from "../user/modules/user.details/UserDetails.Module";
+import { ConfigModule } from "../config/Config.Module";
 
 import { MailServiceProvider } from "./Mail.Service";
 import { ContactRetrievalServiceProvider } from "./ContactRetrieval.Service";
 
 @Module({
-  imports: [UserDetailsModule],
+  imports: [UserDetailsModule, ConfigModule],
   providers: [MailServiceProvider, ContactRetrievalServiceProvider],
   exports: [MailServiceProvider, ContactRetrievalServiceProvider],
 })
