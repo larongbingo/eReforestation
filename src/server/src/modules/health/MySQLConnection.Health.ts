c1f8c075d4ced552ac55af861619f86b33f7bc44
Renz Christen Yeomer A. Pagulayan <larongbingo@gmail.com>
2019-09-20 12:58:32 +0800

    refactor: use new db connection class

--- src/server/src/modules/health/MySQLConnection.Health.ts
+++ src/server/src/modules/health/MySQLConnection.Health.ts
@@ -1,21 +1,20 @@
 import { Injectable, Inject } from "@nestjs/common";
 import { HealthCheckError } from "@godaddy/terminus";
 import { HealthIndicatorResult, HealthIndicator } from "@nestjs/terminus";
-import { Sequelize } from "sequelize-typescript";
 
-import { DatabaseConnectionKey } from "../database/DatabaseConnection";
+import { DatabaseConnection } from "../database/DatabaseConnection";
 
 @Injectable()
 export class MySQLConnectionHealthIndicator extends HealthIndicator {
   constructor(
-    @Inject(DatabaseConnectionKey) private readonly db: Sequelize,
+    @Inject(DatabaseConnection) private readonly db: DatabaseConnection,
   ) {
     super();
   }
 
   public async isHealthy(key: string): Promise<HealthIndicatorResult> {
     try {
-      await this.db.authenticate();
+      await this.db.connection.authenticate();
     } catch (err) {
       throw new HealthCheckError("Cannot query to Database", err.message);
     }
