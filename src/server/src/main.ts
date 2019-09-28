e6c03886773f75ab105167f3c33b5a273ebd5bb5
Renz Christen Yeomer A. Pagulayan <larongbingo@gmail.com>
2019-09-28 14:02:02 +0800

    feat: use swagger on nestjs

--- src/server/src/main.ts
+++ src/server/src/main.ts
@@ -1,4 +1,5 @@
 import { ValidationPipe } from "@nestjs/common";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { NestFactory } from "@nestjs/core";
 import compression = require("compression");
 import helmet = require("helmet");
@@ -11,6 +12,20 @@ import { checkEnvFile } from "./modules/config/Config.Module";
 async function bootstrap() {
   await checkEnvFile();
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle("eReforestation")
+    .setDescription("The APIs of eReforestation website")
+    .setVersion("0.1")
+    .addTag("public")
+    .addTag("participant")
+    .addTag("admin")
+    .addTag("superadmin")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup("api", app, document);
+
   app.useGlobalPipes(new ValidationPipe({ skipMissingProperties: true, transform: true }));
   app.use("/images", express.static("static"));
   app.use(express.json({limit: "100mb"}));
@@ -18,6 +33,7 @@ async function bootstrap() {
   app.use(helmet());
   app.use(cors());
   app.use(compression());
+
   await app.listen(8080);
 }
 bootstrap();
