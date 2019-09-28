4a0948d57dc6ce51f1863e9cf5f9017082a2afd0
Renz Christen Yeomer A. Pagulayan <larongbingo@gmail.com>
2019-09-28 12:13:51 +0800

    feat: add basic index page

--- src/server/src/app.controller.ts
+++ src/server/src/app.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Res } from "@nestjs/common";
+import { Response } from "express";
+import { join } from "path";
 
 @Controller("/")
 export class AppController {
 
   @Get("/")
-  public async getIndex() {
-    return "Hello";
+  public async getIndex(@Res() res: Response) {
+    return res.sendFile(join(__dirname, "index.html"));
   }
 
 }
