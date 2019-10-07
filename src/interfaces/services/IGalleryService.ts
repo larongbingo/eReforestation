4da5cf31c5498fae57abec8ea81cfe24f49041ae
Renz Christen Yeomer A. Pagulayan <larongbingo@gmail.com>
2019-10-07 18:00:22 +0800

    chore: add todo

--- src/interfaces/services/IGalleryService.ts
+++ src/interfaces/services/IGalleryService.ts
@@ -2,6 +2,7 @@
 export const IGalleryService = "IGalleryService";
 export interface IGalleryService {
 
+  // TODO: Replace params with File Type
   storeImage(imageBuffer: Buffer, originalFileName: string): Promise<string>;
 
   getAllImagesNames(): Promise<string[]>;
