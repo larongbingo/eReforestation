196e07d1635058c7cb5e18950d0b83dc223b0f09
Renz Christen Yeomer A. Pagulayan <larongbingo@gmail.com>
2019-10-08 16:02:30 +0800

    refactor: use existing route on APIS_ENDPOINTS

--- src/client/src/pages/newsDetails.tsx
+++ src/client/src/pages/newsDetails.tsx
@@ -3,13 +3,13 @@ import { match } from "react-router-dom";
 import { Container } from "react-bootstrap";
 
 import { NewsDetails } from "../components/NewsDetails";
-import { APIS_ENDPOINT_ROOT } from "../config/endpoints";
+import { APIS_ENDPOINTS } from "../config/endpoints";
 
 export const NewsDetailsPage: FunctionComponent<NewsDetailsPageProps> = ({match}) => {
   const [newsDetails, setNewsDetails] = useState<any>(null);
 
   const getNewsDetails = async () => {
-    const newsDetailsRes = await fetch(APIS_ENDPOINT_ROOT + "/news/details?id=" + match.params.newsId);
+    const newsDetailsRes = await fetch(`${APIS_ENDPOINTS.news.newsDetails.route}/?id=${match.params.newsId}`);
     const newsDetails = await newsDetailsRes.json();
     return newsDetails;
   };
