08df357d7eb6b2df7771b6370a5f2667fe75571f
Renz Christen Yeomer A. Pagulayan <larongbingo@gmail.com>
2019-10-09 13:43:21 +0800

    feat: news card component

---
+++ src/client/src/components/news/NewsCard.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+import { APIS_ENDPOINTS, CLIENT_HOST_NAME } from "../../config/endpoints";
+import { INews } from "../../../../interfaces/models/INews";
+
+export const NewsCard: React.FC<NewsCardProps> = ({news}) => (
+  <Card>
+    <Card.Img
+      variant="top"
+      src={`${APIS_ENDPOINTS.staticFiles.route}/${news.featureImage}`}
+      className="d-block w-100"
+    />
+    <Card.Body>
+      <Card.Title>{news.headline}</Card.Title>
+      <Card.Subtitle>{new Date(news.createdAt!).toLocaleDateString()}</Card.Subtitle>
+      <Card.Text>
+        {news.content.slice(0, 150)}
+      </Card.Text>
+      <Card.Link href={`${CLIENT_HOST_NAME}/news/${news.id}`}>Details</Card.Link>
+    </Card.Body>
+  </Card>
+);
+
+export type NewsCardProps = {
+  news: INews;
+};
