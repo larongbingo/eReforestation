3439ba655e302b37cf4ab279d3f634d698e5b494
Renz Christen Yeomer A. Pagulayan <larongbingo@gmail.com>
2019-12-03 03:20:31 +0800

    feat: use ejs view engine

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { join } from "path";
+
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.setBaseViewsDir(join(__dirname, "..", "views"));
+  app.useStaticAssets(join(__dirname, "..", "static"));
+  app.setViewEngine("ejs");
+
   await app.listen(3000);
 }
 bootstrap();
